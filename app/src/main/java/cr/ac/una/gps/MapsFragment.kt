621d6070a6e971f3ba1e8c1701427e7fc997af3b
EstebanRoHe <esteban_rojashe@hotmail.com>
2023-04-14 13:29:31 -0600

    Update posicition
    Update posicition

--- app/src/main/java/cr/ac/una/gps/MapsFragment.kt
+++ app/src/main/java/cr/ac/una/gps/MapsFragment.kt
@@ -17,26 +17,25 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.Circle
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import java.util.*
 
 class MapsFragment : Fragment() {
-
     private lateinit var map: GoogleMap
     private var titulo: String = ""
-    private lateinit var locationEditText : EditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val prefs = requireActivity().getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         titulo = prefs.getString("marker_label", "") ?: ""
-    }
 
+    }
     @SuppressLint("MissingPermission")
     private val callback = OnMapReadyCallback { googleMap ->
         map = googleMap
-        //Da permisos
+        //Se dan los permisos
+
         if (ActivityCompat.checkSelfPermission(requireContext(),  android.Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED &&
             ActivityCompat.checkSelfPermission(requireContext(),  android.Manifest.permission.ACCESS_COARSE_LOCATION)
@@ -46,7 +45,7 @@ class MapsFragment : Fragment() {
                 1)
             return@OnMapReadyCallback
         }
-        // Posicionar el marcador en la ubicación actual y le da el titulo al punto
+        // busca la posicion actual
         val fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
         fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
             location?.let {
@@ -56,54 +55,55 @@ class MapsFragment : Fragment() {
                     markerOptions.title(titulo)
                 }
                 map.addMarker(markerOptions)
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(miPosicion, 0f))
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(miPosicion, 15f))
             }
         }
 
+        map.setOnMapClickListener { latLng ->
+            // Actualizar posición del marcador con un solo click
+            val markerOptions = MarkerOptions().position(latLng)
+            if (titulo.isNotEmpty()) {
+                markerOptions.title(titulo)
+            }
+            map.clear()
+            map.addMarker(markerOptions)
+
+        }
+
+        map.setOnMapLongClickListener {
+                latLng ->
+            // Actualizar posición del marcador con un long solo click
+            val markerOptions = MarkerOptions().position(latLng)
+            if (titulo.isNotEmpty()) {
+                markerOptions.title(titulo)
+            }
+            map.clear()
+            map.addMarker(markerOptions)
+
+        }
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_maps, container, false)
     }
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
 
-        //buscar por los edite text
-        locationEditText = view.findViewById(R.id.editar_location_longitud)
-        val latitudEditText = view.findViewById<EditText>(R.id.editar_location_latitud)
-        val buscarButton = view.findViewById<Button>(R.id.btn_Buscar)
-        buscarButton.setOnClickListener {
-            val longitud = locationEditText.text.toString().toDoubleOrNull()
-            val latitud = latitudEditText.text.toString().toDoubleOrNull()
-
-            if (longitud != null && latitud != null) {
-                val ubicacion = LatLng(latitud, longitud)
-                map.addMarker(MarkerOptions().position(ubicacion).title(titulo))
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(ubicacion, 0f))
-            } else {
-                Toast.makeText(requireContext(), "Ingrese una ubicación válida", Toast.LENGTH_SHORT).show()
-            }
+        //actualiza el fragment con la nueva pocision
+        var actualButton = view.findViewById<Button>(R.id.btnActual)
+        actualButton.setOnClickListener {
+            map.clear()
+            val mapNuevoFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+            mapNuevoFragment?.getMapAsync(callback)
         }
-
 
     }
 
+
 }
-
-/*
-latitud, longitud
-val sydney = LatLng(-34.0, 151.0)
-            googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-
- */
-
-
 
 
 
